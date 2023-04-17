f95a7bc34856eb0a8856eb339f99af39ec0007f1
recnepspencer <goldenspencerh@gmail.com>
2023-04-17 17:25:10 -0600

    finished registering

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -13,11 +13,22 @@ const register = async(userData) => {
 
 }
 
- const logout = () => {
+const login = async(userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
+
+    if(response.data){
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+    return response.data
+
+}
+
+ export const logout = () => {
     localStorage.removeItem('user')
  }
 const authService = {
     register,
-    logout
+    logout,
+    login
 }
 export default authService
